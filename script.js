0b92636e5c5623515e9e36187a7520e1929bc8db
lambrasas <linas.ambrasas@outlook.com>
2023-07-03 01:26:38 +0300

    Responsive design complete

---
+++ script.js
@@ -0,0 +1,9 @@
+const burger = document.getElementById("burger")
+const links = document.getElementById("links")
+
+
+
+
+burger.addEventListener("click", function(){
+    links.classList.toggle("translate")
+})
