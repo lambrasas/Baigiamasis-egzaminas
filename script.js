b47e674f1f54c7f65ad0da5475949327a383993a
lambrasas <linas.ambrasas@outlook.com>
2023-07-04 01:21:04 +0300

    scrolling to section added

--- script.js
+++ script.js
@@ -2,8 +2,28 @@ const burger = document.getElementById("burger")
 const links = document.getElementById("links")
 
 
-
-
 burger.addEventListener("click", function(){
     links.classList.toggle("translate")
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".animation");
+    const navHeight = document.querySelector("nav").offsetHeight;
+  
+    navLinks.forEach(function(link) {
+      link.addEventListener("click", function(event) {
+        event.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        const targetOffsetTop = target.offsetTop - navHeight;
+        window.scrollTo({
+          top: targetOffsetTop,
+          behavior: "smooth"
+        });
+      });
+    });
+  });
+  
+
+  
+  
+  
